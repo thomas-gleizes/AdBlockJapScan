ec0a2db5832369931a1a65d88321c89f554f7e4a
thomas <thomasgleizes34@gmail.com>
2020-10-29 14:02:04 +0100

    add class

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+const selectors = ['.FbsYi', '.vdo_floating']
 
-const selectors = ['.FbsYi']
+const deleteAdd = () => {
+    selectors.forEach(selector => {
+        let adds = document.querySelectorAll(selector)
+        adds.forEach(add => {
+            if (add.innerHTML !== '') console.log(add)
+            add.innerHTML = ''
+            add.src = '';
+        })
+    });
+}
 
-selectors.forEach(selector => {
-    let adds = document.querySelectorAll(selector)
-    adds.forEach(add => {
-        add.innerHTML = ''
-        add.src = '';
-    })
-});
+deleteAdd()
+
+window.setInterval(deleteAdd, 1000)
