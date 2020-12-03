ed3755b5caa58ce43a6f914ebce8cbb2212f313a
thomas <thomas-gleizes>
2020-12-03 17:43:56 +0100

    add sync storage

--- app.js
+++ app.js
@@ -2,22 +2,25 @@
 
 let selectors = [];
 
-// chrome.storage.sync.get(['selectors'], (value) => {
-//     selectors = value.key;
-// });
+console.log("ADBLOCK JAPSCAN ================================")
+
+chrome.storage.sync.get(['selectors'], result => {
+    selectors = result.selectors
+    console.log(selectors)
+    deleteAdd()
+    window.setInterval(deleteAdd, 1000)
+});
 
 
-// const deleteAdd = () => {
-//     selectors.forEach(selector => {
-//         let adds = document.querySelectorAll(selector)
-//         adds.forEach(add => {
-//             if (add.innerHTML !== '') console.log(add)
-//             add.style.display = 'none'
-//             add.innerHTML = ''
-//             add.src = ''
-//         })
-//     });
-// }
+const deleteAdd = () => {
+    selectors.forEach(selector => {
+        let adds = document.querySelectorAll(selector)
+        adds.forEach(add => {
+            if (add.innerHTML !== '') console.log(add)
+            add.style.display = 'none'
+            add.innerHTML = ''
+            add.src = ''
+        })
+    });
+}
 
-// deleteAdd()
-// window.setInterval(deleteAdd, 1000)
--- popUp.js
+++ popUp.js
@@ -1,14 +1,42 @@
 
 console.log("test")
+const key = "selectors";
+
+
 
 
 document.querySelector("#AddSelector").addEventListener('submit', event => {
     event.preventDefault()
     let data = new FormData(event.target)
+    let selector = data.get('type') + data.get('selector')
 
-    data.forEach(r => {
-        console.log(r)
-    })
+    chrome.storage.sync.get(["selectors"], result => {
+        let tab = [];
+        if (typeof result.selectors == "undefined"){
+            tab.push(selector);
+            console.log(tab)
+            chrome.storage.sync.set({"selectors": tab})
+        } else {
+            tab = result.selectors
 
-
+            console.log(tab)
+            if (!tab.includes(selector)){
+                tab.push(selector);
+                chrome.storage.sync.set({"selectors": tab})
+            }
+        }
+    });
 })
+
+
+
+
+/*
+ chrome.storage.sync.set({key: value}, function() {
+          console.log('Value is set to ' + value);
+        });
+
+ chrome.storage.sync.get(['key'], function(result) {
+          console.log('Value currently is ' + result.key);
+ });
+ */
