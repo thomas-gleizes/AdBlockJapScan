7101cf9b4cc5752422e90519c01fa4e439e821a2
thomas <thomas-gleizes>
2020-12-04 08:36:29 +0100

    add sync storage

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    //chrome.storage.sync.clear()
-    //chrome.storage.sync.set({"selectors": []});
-    chrome.storage.sync.get(['selectors'], result => console.log(result))
-})
--- popUp.js
+++ popUp.js
@@ -1,10 +1,4 @@
 
-console.log("test")
-const key = "selectors";
-
-
-
-
 document.querySelector("#AddSelector").addEventListener('submit', event => {
     event.preventDefault()
     let data = new FormData(event.target)
@@ -14,29 +8,43 @@ document.querySelector("#AddSelector").addEventListener('submit', event => {
         let tab = [];
         if (typeof result.selectors == "undefined"){
             tab.push(selector);
-            console.log(tab)
             chrome.storage.sync.set({"selectors": tab})
         } else {
             tab = result.selectors
-
-            console.log(tab)
             if (!tab.includes(selector)){
                 tab.push(selector);
                 chrome.storage.sync.set({"selectors": tab})
+            } else {
+                alert("Le selectors existe déja.")
             }
         }
     });
 })
 
+document.querySelector("#h-liste > .btn").addEventListener('click', event => {
+    event.target.style.display = "none"
+    generateListe()
+})
 
+const generateListe = () => {
+    let list = document.querySelector('div#list')
+    list.innerHTML = "";
+    chrome.storage.sync.get(["selectors"], result => {
+        if (typeof result.selectors !== "undefined") {
+            result.selectors.forEach(selector => {
+                let img = document.createElement('img')
+                list.innerHTML += `<div> ${selector} <img onclick="deleteSelector('${selector}')" src='clear.svg' alt='clear'/></div>`
+            })
+        }
+    })
+}
 
-
-/*
- chrome.storage.sync.set({key: value}, function() {
-          console.log('Value is set to ' + value);
-        });
-
- chrome.storage.sync.get(['key'], function(result) {
-          console.log('Value currently is ' + result.key);
- });
- */
+const deleteSelector = selector => {
+    chrome.storage.sync.get(["selectors"], result => {
+        let tab = result.selectors
+        if (typeof tab !== "undefined" && tab.includes(selector)){
+            tab.splice(tab.indexOf(selector, 1))
+            console.log(tab);
+        }
+    })
+}
